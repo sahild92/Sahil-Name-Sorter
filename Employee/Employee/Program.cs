bd195489aee41db7ac165635d6f17058a3670a8b
VIC\svd <sahil.deshpande@gadens.com>
2018-09-10 13:01:13 +1000

    perform name fixes

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -9,7 +9,7 @@ namespace Employee
         static void Main(string[] args)
         {
             // Create the IEnumerable data source  
-            string[] lines = System.IO.File.ReadAllLines(@"C:\repos\internal\Internal\ConsoleApp5\unsorted-names-list.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\repos\internal\Internal\Employee\unsorted-names-list.txt");
 
             // Create the query. Put field 2 first, then  
             // reverse and combine fields 0,1,2 and 3 from the old field  
@@ -19,7 +19,7 @@ namespace Employee
                 orderby x[3]
                 select x[3] + ", " + x[2] + ", " + x[1] + ", " + x[0];
             // Execute the query and write out the new file. 
-            System.IO.File.WriteAllLines(@"C:\repos\internal\Internal\ConsoleApp5\sorted-names-list.txt", query.ToArray());
+            System.IO.File.WriteAllLines(@"C:\repos\internal\Internal\Employee\sorted-names-list.txt", query.ToArray());
             Console.WriteLine("Sorted names are written to disk. Press any key to exit");
             Console.ReadKey();
         }

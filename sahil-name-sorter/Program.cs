dac2fe037739ff10a4b9732df85b42ba8c0fc2eb
VIC\svd <sahil.deshpande@gadens.com>
2018-09-11 10:08:56 +1000

    Remove duplicate solution file and make fixes to sahil-name-sorter

--- sahil-name-sorter/Program.cs
+++ sahil-name-sorter/Program.cs
@@ -10,7 +10,7 @@ namespace SahilNameSorter
         {
             Console.WriteLine("Press a key to Sort Last Name");
                 // Create the IEnumerable data source  
-                string[] lines = System.IO.File.ReadAllLines(@"Employee Files\unsorted-names-list.txt");
+                string[] lines = System.IO.File.ReadAllLines(@"unsorted-names-list.txt");
             Console.ReadKey();
             // Create the query. Put field 2 first, then  
             // reverse and combine fields 0,1,2 and 3 from the old field  
@@ -22,7 +22,7 @@ namespace SahilNameSorter
             //Print the query output on console
             Console.WriteLine("{0}", string.Join(", ", query.ToArray()));
             // Execute the query and write out the new file. 
-            System.IO.File.WriteAllLines(@"Employee Files\sorted-names-list.txt", query.ToArray());
+            System.IO.File.WriteAllLines(@"sorted-names-list.txt", query.ToArray());
             Console.WriteLine("Sorted names are written to file. Press any key to exit");
             
             Console.ReadKey();
--- sahil-name-sorter/Properties/AssemblyInfo.cs
+++ sahil-name-sorter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp5")]
+[assembly: AssemblyTitle("name-sorter.exe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp5")]
+[assembly: AssemblyProduct("name-sorter.exe")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

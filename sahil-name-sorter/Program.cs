47b6db7958c1c429aa68cb24aab3450a52ef88e7
VIC\svd <sahil.deshpande@gadens.com>
2018-09-13 15:58:37 +1000

    Add lambda function to sahil-name-sorter

--- sahil-name-sorter/Program.cs
+++ sahil-name-sorter/Program.cs
@@ -8,25 +8,53 @@ namespace SahilNameSorter
     {
         static void Main(string[] args)
         {
-                 Console.WriteLine("Press a key to Sort Last Name");
-                  // Create the IEnumerable data source  
-                  var lines = System.IO.File.ReadAllLines(@"unsorted-names-list.txt");
-                    Console.ReadKey();
+            Console.WriteLine("Press a key to Sort Last Name");
+            // Create the IEnumerable data source  
+            string[] lines = System.IO.File.ReadAllLines(@"unsorted-names-list.txt");
+            Console.ReadKey();
             // Create the query. Put field 2 first, then  
             // reverse and combine fields 0,1,2 and 3 from the old field  
-            IEnumerable<Person> query =
-            from line in lines
-            select new Person(line);
-            var people = query.ToList();
-            var query2 = from person in people
-                         orderby person.Surname
-                         select person.FullName;
+            /*   IEnumerable<Person> query =
+               from line in lines
+               select new Person(line);
+               var people = query.ToList();
+               var query2 = from person in people
+                            orderby person.Surname
+                            select person.FullName; */
+            var people = new List<Person>();
+            foreach (var line in lines)
+            {
+                people.Add(new Person(line));
+            } 
+            var sortedPeople = people.OrderBy(x => x.Surname).ToList();
             //Print the query output on console
             // Execute the query and write out the new file. 
-             System.IO.File.WriteAllLines(@"sorted-names-list.txt", query2.ToArray());
+            
+
+            System.IO.File.WriteAllLines(@"sorted-names-list.txt", GetFullNames(sortedPeople));
             Console.WriteLine("Sorted names are written to file. Press any key to exit");
-                Console.ReadKey();
+            Console.ReadKey();
+        }
+
+        public static List<string> GetFullNames(List<Person> people)
+        {
+            var sortedlines = new List<string>();
+
+            foreach (var person in people)
+            {
+                sortedlines.Add(person.FullName);
             }
-       
+
+            return sortedlines;
+        }
+
+  
+
+
+          
+    
+
+        
+
     }
 }

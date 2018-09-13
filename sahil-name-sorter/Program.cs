2ec50f57913ea1a164fd5ef86c8f7135c4d30a92
VIC\svd <sahil.deshpande@gadens.com>
2018-09-13 16:51:20 +1000

    Refactor with new class SortBySurname in sahil-name-sorter

--- sahil-name-sorter/Program.cs
+++ sahil-name-sorter/Program.cs
@@ -10,7 +10,7 @@ namespace SahilNameSorter
         {
             Console.WriteLine("Press a key to Sort Last Name");
             // Create the IEnumerable data source  
-            string[] lines = System.IO.File.ReadAllLines(@"unsorted-names-list.txt");
+            var lines = System.IO.File.ReadAllLines(@"unsorted-names-list.txt").ToList();
             Console.ReadKey();
             // Create the query. Put field 2 first, then  
             // reverse and combine fields 0,1,2 and 3 from the old field  
@@ -21,40 +21,40 @@ namespace SahilNameSorter
                var query2 = from person in people
                             orderby person.Surname
                             select person.FullName; */
-            var people = new List<Person>();
-            foreach (var line in lines)
-            {
-                people.Add(new Person(line));
-            } 
-            var sortedPeople = people.OrderBy(x => x.Surname).ToList();
+            /*  var people = new List<Person>();
+                       foreach (var line in lines)
+                       {
+                           people.Add(new Person(line));
+                       } 
+                       var sortedPeople = people.OrderBy(x => x.Surname).ToList();   */
             //Print the query output on console
             // Execute the query and write out the new file. 
-            
-
-            System.IO.File.WriteAllLines(@"sorted-names-list.txt", GetFullNames(sortedPeople));
+            var sortedNames = SortBySurname(lines);
+            System.IO.File.WriteAllLines(@"sorted-names-list.txt", GetFullNames(sortedNames));
             Console.WriteLine("Sorted names are written to file. Press any key to exit");
             Console.ReadKey();
         }
-
         public static List<string> GetFullNames(List<Person> people)
         {
             var sortedlines = new List<string>();
-
             foreach (var person in people)
             {
                 sortedlines.Add(person.FullName);
             }
-
             return sortedlines;
         }
 
-  
-
-
-          
-    
-
+        public static List<Person> SortBySurname(List<string> lines)
+        {
+            var people = new List<Person>();
+            foreach (var line in lines)
+            {
+                people.Add(new Person(line));
+            }
+            var sortedPeople = people.OrderBy(x => x.Surname).ToList();
+            return sortedPeople;
+        }
         
-
+     
     }
 }

934b00dc0d9b5bb418f58e053eaa7b2cdec5ba3c
VIC\svd <sahil.deshpande@gadens.com>
2018-09-13 10:39:59 +1000

    Add new query to sahil-name-sorter

--- sahil-name-sorter/Properties/AssemblyInfo.cs
+++ sahil-name-sorter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("name-sorter.exe")]
+[assembly: AssemblyTitle("name-sorter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("name-sorter.exe")]
+[assembly: AssemblyProduct("name-sorter")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

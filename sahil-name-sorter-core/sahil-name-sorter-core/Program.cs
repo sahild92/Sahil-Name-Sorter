39115e829791017f21822c8a952cd7a740b5dedf
VIC\svd <sahil.deshpande@gadens.com>
2018-09-19 14:49:08 +1000

    Add sort by decending feature (If else block) to sahil-name-sorter

--- sahil-name-sorter-core/sahil-name-sorter-core/Program.cs
+++ sahil-name-sorter-core/sahil-name-sorter-core/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using SahilNameSorter.Domain;
 using SahilNameSorter.Services;
-
 namespace SahilNameSorter
 {
     class Program
@@ -14,6 +13,8 @@ namespace SahilNameSorter
             // Create the IEnumerable data source  
             var lines = System.IO.File.ReadAllLines(@"unsorted-names-list.txt").ToList();
             Console.ReadKey();
+            Console.WriteLine("Press A for names in Ascending OR Press D for names in Decending");
+            string s1 = Console.ReadLine();
             //Print the query output on console
             // Execute the query and write out the new file.
             var peopleService = new PersonService();
@@ -22,10 +23,28 @@ namespace SahilNameSorter
             {
                 people.Add(new Person(line));
             }
-            var sortedNames = peopleService.Sort(people);
-            System.IO.File.WriteAllLines(@"sorted-names-list.txt", PersonService.GetFullNames(sortedNames));
-            Console.WriteLine("Sorted names are written to file. Press any key to exit");
-            Console.ReadKey();
+            
+                var sortedNames = peopleService.Sort(people);
+          
+                var sortedNamesDec = peopleService.SortbyDecending(people);
+
+            if (s1 == "A")
+            {
+                System.IO.File.WriteAllLines(@"sorted-names-list.txt", PersonService.GetFullNames(sortedNames));
+                Console.WriteLine("Sorted names are written to file. Press any key to exit");
+                Console.ReadKey();
+            }
+            else if (s1 == "D")
+            {
+                System.IO.File.WriteAllLines(@"sorted-names-list.txt", PersonService.GetFullNames(sortedNamesDec));
+                Console.WriteLine("Sorted names are written to file. Press any key to exit");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Selection");
+                Console.ReadKey();
+            }
         }
     }
 }

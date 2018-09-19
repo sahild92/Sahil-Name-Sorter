94ca2034991730698b38af0eea35df2e344e6bca
VIC\svd <sahil.deshpande@gadens.com>
2018-09-19 15:10:33 +1000

    Add new test project and test cases to sahil-name-sorter-core-test

---
+++ sahil-name-sorter-core/sahil-name-sorter-core-tests/sahil-name-sorter-core-tests.cs
@@ -0,0 +1,27 @@
+﻿using SahilNameSorter.Domain;
+using System;
+using Xunit;
+
+namespace SahilNameSorter.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void ShouldHaveSurname()
+        {
+            //Arange & act
+            var s = new Person("Sahil Deshpande");
+
+            //assert
+            Assert.Equal("Deshpande", s.Surname);
+
+        }
+        [Fact]
+        public void Nullcheck()
+        {
+            var s = new Person("Buth Arthur Post Archer");
+
+            Assert.NotNull(s.Surname);
+        }
+    }
+}

aac3496780b69255d9bd08f5ed4fbb96fca651fc
VIC\svd <sahil.deshpande@gadens.com>
2018-09-26 14:31:40 +1000

    Add exceptions for ascending, decending and firstname, lastname in sahil-name-sorter-core

--- sahil-name-sorter-core/sahil-name-sorter-core/Program.cs
+++ sahil-name-sorter-core/sahil-name-sorter-core/Program.cs
@@ -7,16 +7,15 @@ using SahilNameSorter.Domain;
 using SahilNameSorter.Services;
 using Microsoft.Extensions.CommandLineUtils;
 using System.Reflection;
+
 namespace SahilNameSorter
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             var app = new CommandLineApplication();
             app.Name = "sahil-name-sorter-core";
-
 
             var basicOption = app.Option("-fp|--filepath",
                    "provides the name file for input",
@@ -43,39 +42,44 @@ namespace SahilNameSorter
             app.VersionOption("-v|--version", () => {
                 return string.Format("Version {0}", Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
             });
-
+            ;
             app.OnExecute(() =>
-            {
+              {
                 Console.WriteLine("simple-command is executing");
+                if (firstnameOption.HasValue() && lastnameOption.HasValue())
+                   {
+                      throw new Exception("Cannot specify the first name and last name together");
+                   }
 
-                Run(basicOption, firstnameOption.HasValue(), lastnameOption.HasValue(), NameAscendingOption.HasValue(), NameDecendingOption.HasValue());
+                if (NameAscendingOption.HasValue() && NameAscendingOption.HasValue())
+                   {
+                      throw new Exception("Cannot specify the ascending and decending together");
+                   }
+            Run(basicOption, firstnameOption.HasValue(), lastnameOption.HasValue(), NameAscendingOption.HasValue(), NameDecendingOption.HasValue());
 
                 Console.WriteLine("simple-command has finished.");
                 return 0; //return 0 on a successful execution
-            });
-
-            Console.WriteLine("Press a key to Sort the Names");
+                });
 
             // Create the IEnumerable data source
             try
-            {
-                // This begins the actual execution of the application
-                Console.WriteLine("ConsoleArgs app executing...");
-                app.Execute(args);
+                {
+                    // This begins the actual execution of the application
+                    Console.WriteLine("ConsoleArgs app executing...");
+                    app.Execute(args);
+                }
+                catch (CommandParsingException ex)
+                {
+                    // You'll always want to catch this exception, otherwise it will generate a messy and confusing error for the end user.
+                    // the message will usually be something like:
+                    // "Unrecognized command or argument '<invalid-command>'"
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unable to execute application: {0}", ex.Message);
+                }
             }
-            catch (CommandParsingException ex)
-            {
-                // You'll always want to catch this exception, otherwise it will generate a messy and confusing error for the end user.
-                // the message will usually be something like:
-                // "Unrecognized command or argument '<invalid-command>'"
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Unable to execute application: {0}", ex.Message);
-            }
-
-        }
         private static void Run(CommandOption basicOption, bool firstnameOption, bool lastnameOption, bool NameAscendingOption, bool NameDecendingOption)
         {
             var lines = File.ReadAllLines(basicOption.Value(), Encoding.UTF7).ToList();
@@ -88,10 +92,9 @@ namespace SahilNameSorter
                 people.Add(new Person(line));
             }
             var sortedNames = new List<Person>();
-
+           
             if (firstnameOption)
             {
-
                 if (NameAscendingOption)
                 {
                     INameSorter namesorter = new NameSorterAscending(x => x.FirstName);
@@ -102,16 +105,9 @@ namespace SahilNameSorter
                     INameSorter namesorter = new NameSorterDecending(x => x.FirstName);
                     sortedNames = namesorter.Sort(people);
                 }
-                else
-                {
-                    Console.WriteLine("Invalid Selection");
-                    Console.ReadKey();
-                }
-
             }
             else if (lastnameOption)
             {
-
                 if (NameAscendingOption)
                 {
                     INameSorter namesorter = new NameSorterAscending(x => x.Surname);
@@ -121,11 +117,6 @@ namespace SahilNameSorter
                 {
                     INameSorter namesorter = new NameSorterDecending(x => x.Surname);
                     sortedNames = namesorter.Sort(people);
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Selection");
-                    Console.ReadKey();
                 }
             }
             File.WriteAllLines(@"sorted-names-list.txt", PersonService.GetFullNames(sortedNames));

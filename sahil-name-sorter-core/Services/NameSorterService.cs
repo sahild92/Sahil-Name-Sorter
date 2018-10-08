d23628ddd2736c533ffdff31d33c4ff3677f0154
VIC\svd <sahil.deshpande@gadens.com>
2018-10-08 16:06:42 +1100

    General fixes

--- sahil-name-sorter-core/Services/NameSorterService.cs
+++ sahil-name-sorter-core/Services/NameSorterService.cs
@@ -69,8 +69,10 @@ namespace SahilNameSorterCore.Services
                 {
                     var jsonResponse = await client.GetGender(person.FirstName);
                     var genderObject = JsonConvert.DeserializeObject<GenderizeResult>(jsonResponse);
-                    person.Gender = genderObject.gender;
-                    var saveCache = _cache.Set(person.FirstName, genderObject);
+                    
+                    
+                        person.Gender = genderObject.gender;
+                        var saveCache = _cache.Set(person.FirstName, genderObject);
                     
                 }
                 else

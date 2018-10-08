ea8ab3eb7172bc714c4e057608022d13d33f9ca0
VIC\svd <sahil.deshpande@gadens.com>
2018-10-09 10:28:58 +1100

    add app insights

--- sahil-name-sorter-web/Program.cs
+++ sahil-name-sorter-web/Program.cs
@@ -16,6 +16,7 @@ namespace sahilNameSorterWeb
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }

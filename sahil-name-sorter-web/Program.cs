997c2b75d8fd583531dec7d0054dcd535107a825
VIC\svd <sahil.deshpande@gadens.com>
2018-10-09 11:27:57 +1100

    set app insights instrumentation key at host builder creation time

--- sahil-name-sorter-web/Program.cs
+++ sahil-name-sorter-web/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace sahilNameSorterWeb
 {
@@ -8,15 +9,24 @@ namespace sahilNameSorterWeb
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            // Adding JSON file into IConfiguration.
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                //.AddUserSecrets("0")
+                .Build();
+
+            // Read instrumentation key from IConfiguration.
+            string ikey = config["ApplicationInsights:InstrumentationKey"];
+
+            CreateWebHostBuilder(args, ikey).Build().Run();
             
         }
 
       
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, string appInsightsKey) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseApplicationInsights()
+                .UseApplicationInsights(appInsightsKey)
                 .UseStartup<Startup>();
     }
 }

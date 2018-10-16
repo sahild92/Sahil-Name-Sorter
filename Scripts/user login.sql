5dc70cfce9d527e29f9900df97886eeb952a637d
VIC\svd <sahil.deshpande@gadens.com>
2018-10-16 11:29:09 +1100

    Add Azure sql server for sahil name sorter

---
+++ Scripts/user login.sql
@@ -0,0 +1,7 @@
+-- ======================================================================================
+-- Create SQL Login template for Azure SQL Database and Azure SQL Data Warehouse Database
+-- ======================================================================================
+
+CREATE LOGIN  namesorterapp 
+	WITH PASSWORD = 'Coolsaz1234' 
+GO
